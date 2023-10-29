b23567626cacc234f25ae91b11a0af8ccdbaaecf
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-29 13:28:04 +0300

    Project customization

---
+++ PhotosApp/Sources/Controllers/AlbumsViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+import SnapKit
+
+class AlbumsViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: - Setup
+    
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+}
+
