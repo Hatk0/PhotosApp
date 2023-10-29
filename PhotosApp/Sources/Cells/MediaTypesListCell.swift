9488f5362dae1add69d93f53f43f88b29fabd7b1
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-29 13:57:20 +0300

    Fill MediaTypesListCell with data

--- PhotosApp/Sources/Cells/MediaTypesListCell.swift
+++ PhotosApp/Sources/Cells/MediaTypesListCell.swift
@@ -7,7 +7,28 @@ class MediaTypesListCell: UICollectionViewListCell {
     
     // MARK: - UI
     
-    // MARK: - Initalizers
+    private lazy var settingImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .center
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private lazy var titlelabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.textColor = .systemBlue
+        return label
+    }()
+    
+    private lazy var quantityLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    // MARK: - Initializers
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,16 +43,33 @@ class MediaTypesListCell: UICollectionViewListCell {
     // MARK: - Setup
     
     private func setupHierarchy() {
-
+        contentView.addSubview(settingImageView)
+        contentView.addSubview(titlelabel)
+        contentView.addSubview(quantityLabel)
     }
     
     private func setupLayout() {
-
+        settingImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+            make.width.equalTo(32)
+            make.height.equalTo(35)
+        }
+        
+        titlelabel.snp.makeConstraints { make in
+            make.leading.equalTo(settingImageView.snp.trailing).offset(16)
+            make.centerY.equalToSuperview()
+        }
+        
+        quantityLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview().offset(150)
+            make.centerY.equalToSuperview()
+        }
     }
     
-    // MARK: - Configuration
-    
     func configuration(model: AlbumsModel) {
-
+        settingImageView.image = UIImage(systemName: model.image)
+        titlelabel.text = model.title
+        quantityLabel.text = model.quantity
     }
 }
