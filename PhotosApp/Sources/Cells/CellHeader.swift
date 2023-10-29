a4bc5843ad1832fce31167560140af262666c8a6
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-29 13:41:06 +0300

    Fill CellHeader with data

--- PhotosApp/Sources/Cells/CellHeader.swift
+++ PhotosApp/Sources/Cells/CellHeader.swift
@@ -1,8 +1,67 @@
-//
-//  CellHeader.swift
-//  PhotosApp
-//
-//  Created by Dmitry Yastrebov on 29.10.2023.
-//
+import UIKit
+import SnapKit
 
-import Foundation
+class CellHeader: UICollectionReusableView {
+    
+    static let identifier = "CellHeader"
+    
+    // MARK: - UI
+    
+    lazy var title: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 22, weight: .heavy)
+        return label
+    }()
+    
+    private lazy var seeAllButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("See All", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .regular)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        clipsToBounds = true
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Error in Cell")
+    }
+    
+    // MARK: - Setup
+    
+    private func setupHierarchy() {
+        addSubview(title)
+        addSubview(seeAllButton)
+    }
+    
+    private func setupLayout() {
+        title.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().inset(2)
+        }
+
+        seeAllButton.snp.makeConstraints { make in
+            make.centerY.equalTo(title)
+            make.right.equalToSuperview().inset(2)
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        title.text = nil
+    }
+    
+    // MARK: - Action
+    
+    @objc func buttonTapped() {
+        
+    }
+}
