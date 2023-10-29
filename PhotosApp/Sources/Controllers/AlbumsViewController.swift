257729a9a393ccba599241b4c52389d4a3cb1003
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-29 13:31:09 +0300

    Create collectionView

--- PhotosApp/Sources/Controllers/AlbumsViewController.swift
+++ PhotosApp/Sources/Controllers/AlbumsViewController.swift
@@ -5,6 +5,14 @@ class AlbumsViewController: UIViewController {
     
     // MARK: - UI
     
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        return collectionView
+    }()
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -21,11 +29,25 @@ class AlbumsViewController: UIViewController {
     }
     
     private func setupHierarchy() {
-        
+        view.addSubview(collectionView)
     }
     
     private func setupLayout() {
-        
+        collectionView.snp.makeConstraints { make in
+            make.right.bottom.left.equalTo(view)
+            make.top.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 }
 
+extension AlbumsViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        20
+    }
+    
+    
+}
